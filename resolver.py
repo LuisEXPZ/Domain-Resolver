1c80b8ac81163c8d9e595caa63d98bccdaf05ba8
LuisEXPZ <87787474+LuisEXPZ@users.noreply.github.com>
2021-08-16 19:01:08 -0300

    Create resolver.py

---
+++ resolver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import socket,sys
+
+if len(sys.argv) <= 1:
+     print "Modo De Uso:"
+     print "Exemplo: ./resolver.py businesscorp.com.br"
+
+elif len(sys.argv) >= 3:
+     print "Erro limite de argumentos e 1!"
+
+else:
+     host = sys.argv[1]
+     d = socket.gethostbyname(host)
+     print host,"-->",d
+
